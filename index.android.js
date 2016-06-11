7b30b5cda19160431f649af74258562b256aad1e
Christian <palazzochristian@yahoo.it>
2016-06-11 17:09:03 +0200

    fix project init

--- index.android.js
+++ index.android.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-class AwesomeProject extends Component {
+class coOK extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -49,4 +49,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('coOK', () => coOK);
