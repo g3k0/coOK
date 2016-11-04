21b2c38aa50c44c4a39a6ba31087a67307376642
Christian Palazzo <info@christianpalazzo.org>
2016-11-04 12:55:16 +0100

    REST server recipes file text upload and service modification

--- coOK_REST_server/server/models/general-services.js
+++ coOK_REST_server/server/models/general-services.js
@@ -17,19 +17,27 @@
 
 	module.exports = (Generalservices) => {
 
-		Generalservices.importRecipes = (recipes, cb) => {
+		/**
+		 * The service take recipes from a txt file, validate the data and import them into the db
+		 */
+		Generalservices.importRecipes = (cb) => {
 			log.info(`[Generalservices][importRecipes] received request with recipes to import...`);
 			let recipesSchema = require(`${__base}/schemas/recipes.json`);
-			let firstRecipe = recipes[0]; //TO DO - loop the validation to all recipes array request
 
-			requestUtils.validate(recipesSchema, firstRecipe, () => {
+			//TO DO - import txt data from file to JSON object
+
+			//let firstRecipe = recipes[0]; //TO DO - loop the validation to all recipes array request
+
+			/*requestUtils.validate(recipesSchema, firstRecipe, () => {
 				log.info(`[Generalservices][importRecipes] response sent`)
 				return cb(null,{results:'ok'});
 
 			}, (err) => {
 				log.error(`[Generalservices][importRecipes] error: ${err}`);
 				return cb(err);
-			});
+			});*/
+			return cb(null,{results:'ok'});
+
 		};
 
 	};
