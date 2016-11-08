c0cb1130a1ee215c3624ae3ef04cea0ac80aff2e
Christian Palazzo <info@christianpalazzo.org>
2016-11-08 13:06:26 +0100

    auth routes definition

---
+++ coOk!_REST_server/server/boot/handlers/auth.js
@@ -0,0 +1,17 @@
+/** 
+ * File: auth.js
+ * @author: Christian Palazzo
+ * @date: 2016 11 08
+ */
+
+(function () {
+	'use strict';
+
+	let login = () => {};
+	let logout = () => {};
+	let user = () => {};
+
+	module.exports.login = login;
+	module.exports.logout = logout;
+	module.exports.user = user;
+})();
--- coOk!_REST_server/server/boot/root.js
+++ coOk!_REST_server/server/boot/root.js
@@ -1,6 +1,53 @@
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
+/**
+ * Root file. Executed at app boot
+ * @author: Christian Palazzo
+ * @date: 2016 11 08
+ */
+
+(function () {
+	'use strict'
+
+	/**
+	 * Module dependencies
+	 */
+	let auth = require('./handlers/auth');
+	let log = require(`${__base}/lib/logging`);
+	let bodyParser = require('body-parser');
+	let common = require('./handlers/common');
+	let cookieSession = require('cookie-session');
+
+	module.exports = (server) => {
+		/*
+		 * body parser middleware for custom services
+		 */
+	    server.use(bodyParser.json());       // to support JSON-encoded bodies
+	    server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	      extended: true
+	    }));
+
+		/*
+		 * Initialize session middleware
+		 */
+		server.use(cookieSession({
+	    	secret: server.get('cookieSecret')
+	    }));
+
+		// Install a `/` route that returns server status
+		let router = server.loopback.Router();
+
+		/*
+		 * Front end configuration file dynamic generation
+		 */
+		router.get('/js/config.js', common.config);
+
+		/*
+		 * User authentication routes
+		 */
+		router.post('/auth/login', auth.login);
+		router.get('/auth/user', auth.user);
+		router.get('/auth/logout', auth.logout);
+
+		router.get('/', server.loopback.status());
+		server.use(router);
+	};
+})();
