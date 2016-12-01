5d52b7b92ffda2bafcd1a3ef8f86985cccbb6b61
Christian Palazzo <info@christianpalazzo.org>
2016-12-01 14:01:01 +0100

    app login service implementation (to complete)

--- coOk!_REST_server/server/models/appauth.js
+++ coOk!_REST_server/server/models/appauth.js
@@ -24,8 +24,8 @@
 			log.info(`[Appauth][register] received app mobile registration request. Processing...`);
 			let err = new Error();
 
-			if (!token || token !== app.get('mobileApp').initialToken) {
-				log.info(`[Appauth][register] initial token not present or wrong. Stopping registration process...`);
+			if (!token || token === '{token}' || token !== app.get('mobileApp').initialToken) {
+				log.info(`[Appauth][register] initial token not present or wrong. Stopping the registration process...`);
 				err.statusCode = 401;
 				err.message = 'initial token not set or wrong';
 
@@ -35,7 +35,7 @@
 			//we are sure here that service was call by an app. I generate now the app data for the User model.
 			let user = {
 				email: '',
-				password: 'mobileApp',
+				password: app.get('mobileApp').password,
 				group: app.get('mobileApp').group,
 				realm: app.get('mobileApp').realm,
 				appId: ''
@@ -56,6 +56,79 @@
 			    }
 			    log.info(`[Appauth][register] Mobile app registered with success. App id is ${userInstance.appId}`);
 			    return cb(null, {results: userInstance.appId});
+			});
+		};
+
+		//------------------------------------------------------------------------------------------------------------------------------
+
+		/**
+		 * The service takes the app id furnished by the mobile application, extract the user from the users collection, 
+		 * then execute the login and returns the Loopback access token.
+		 */
+		Appauth.login = (appId, cb) => {
+			log.info(`[Appauth][login] received app mobile login request. Processing...`);
+			let err = new Error();
+
+			if (!appId || appId === '{appId}') {
+				log.info(`[Appauth][login] application id not present. Stopping the login process...`);
+				err.statusCode = 401;
+				err.message = 'app id not set';
+
+				return cb(err);
+			}
+
+			//I extract here the user from the collection
+
+			/* Promises declaration */
+			let user = (appId) => {
+				let userPromise = new Promise((resolve, reject) => {
+					app.models.users.findOne({where:{appId: appId}}, (err, user) => {
+						if (err) reject(err);
+						resolve(user);
+					});
+				});
+
+				return userPromise;
+			};
+
+			let loopbackLogin = (email) => {
+				let loopbackLogin = new Promise((resolve, reject) => {
+					app.models.User.login({
+				    	email: email,
+				    	password: app.get('mobileApp').password
+				  	}, 'user', function(err, token) {
+				   		if (err) {
+				      		// TO DO 	
+				    	}
+				    	//TO DO
+				      	//accessToken: token.id
+				    });
+				});
+				
+				return loopbackLoginPromise;
+			};
+			/* End promises declaration */
+
+			user(appId)
+			.then((user) => {
+				//resolve
+				if (!user) {
+					log.info(`[Appauth][login] user not found. Stopping the login process...`);
+					err.status = 404;
+					err.message = 'user not found';
+					return cb(err);
+				}
+				// I have the user object, now I do the Loopback login
+				log.info(`[Appauth][login] user found. Obtaining an access token fot the mobile app...`);
+
+				//TO DO
+				return cb(null, user);
+			}, (err) => {
+				//reject
+				log.error(`[Appauth][login] Error on querying the users collection: ${err}. Stopping the login process...`);
+				err.status = 500;
+				err.message = 'Internal server error';
+				return cb(err);
 			});
 		};
 	};
