73757adf781258a9a2f172ed721131b35dde33d7
Christian Palazzo <info@christianpalazzo.org>
2016-12-11 13:44:54 +0100

    compenents heredity definition fix

--- app/src/components/calendar-detail/calendar-detail.ts
+++ app/src/components/calendar-detail/calendar-detail.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { RecipesListPage } from '../recipes-list/recipes-list';
 
 @Component({
   selector: 'page-calendar-detail',
-  templateUrl: 'calendar-detail.html'
+  templateUrl: 'calendar-detail.html',
+  providers: [
+  	RecipesListPage
+  ]
 })
 export class CalendarDetailPage {
 
--- app/src/components/calendar/calendar.ts
+++ app/src/components/calendar/calendar.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {CalendarDetailPage} from '../calendar-detail/calendar-detail'
-import { RecipesListPage } from '../recipes-list/recipes-list';
 
 @Component({
   selector: 'page-calendar',
   templateUrl: 'calendar.html',
   providers: [
-  	CalendarDetailPage,
-  	RecipesListPage
+  	CalendarDetailPage
   ]
 })
 export class CalendarPage {
