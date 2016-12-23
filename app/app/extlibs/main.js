2b61394f1e5274fc692398a37688afd0307e653b
Christian Palazzo <info@christianpalazzo.org>
2016-12-23 15:10:47 +0100

    filters section implementation

---
+++ app/app/extlibs/main.js
@@ -0,0 +1,11 @@
+//$(document).ready(function () {
+	
+	//filters section accordion
+	setTimeout(function () {
+		$('#accordion').accordion({
+	    	collapsible: true,
+	     	heightStyle: 'content',
+	     	active: false
+	    });
+	}, 1500);
+//});
