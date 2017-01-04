2ba5553560b50ea5c3f2391c2a58279ac3a8a2c4
Christian Palazzo <info@christianpalazzo.org>
2017-01-04 16:43:27 +0100

    search bar and recipes get implementation

--- app/app/pages/favorites/favorites.ts
+++ app/app/pages/favorites/favorites.ts
@@ -1,9 +1,35 @@
 import {Component} from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   templateUrl: 'build/pages/favorites/favorites.html'
 })
 export class FavoritesPage {
-  constructor() {
-  }
+	searchQuery: string = '';
+	items: any[];
+
+	constructor(public http: Http) {
+		this.http.get('./recipes_mock.json')
+		.subscribe(res => {
+			this.items = res.json();
+		});
+  	}
+
+  	getItems(ev: any) {
+    	
+    	// set val to the value of the searchbar
+    	let val = ev.target.value;
+
+    	// if the value is an empty string don't filter the items
+    	if (val && val.trim() != '') {
+      		this.items = this.items.filter((item) => {
+      			return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      		})
+    	} else {
+    		this.http.get('./recipes_mock.json')
+			.subscribe(res => {
+				this.items = res.json();
+			});
+    	}
+  	}
 }
