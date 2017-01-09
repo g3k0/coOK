d2acaf946349201dd9c25d1d365fa93def3222a3
Christian Palazzo <info@christianpalazzo.org>
2017-01-09 17:19:00 +0100

    day detail page list formatting

--- app/app/pages/calendar/calendar.ts
+++ app/app/pages/calendar/calendar.ts
@@ -1,16 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {ModalController} from 'ionic-angular';
+import {RecipePage} from '../recipe/recipe';
 
 @Component({
-  templateUrl: 'build/pages/calendar/calendar.html'
+	templateUrl: 'build/pages/calendar/calendar.html',
+	directives: [RecipePage]
 })
+
 export class CalendarPage {
-  flipped: boolean = false;
+
+	@ViewChild(RecipePage) RecipePage: RecipePage;
+
+	flipped: boolean = false;
  
-  constructor() {
- 
-  }
- 
-  flip(){
-    this.flipped = !this.flipped;
-  }
+	constructor(
+		public modalCtrl: ModalController
+	) {
+ 		
+	}
+ 	
+ 	/**
+ 	 * Flip the day detail page
+ 	 */
+	flip () {
+    	this.flipped = !this.flipped;
+  	}
+
+  	/**
+     * Modal page loading method
+     */
+    presentModal () {
+        let modal = this.modalCtrl.create(RecipePage);
+        modal.present();
+    }
 }
