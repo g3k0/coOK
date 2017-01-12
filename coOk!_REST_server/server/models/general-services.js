0de100ef84a999b927ce08de04c77cf26d6cb789
Christian Palazzo <info@christianpalazzo.org>
2017-01-12 14:03:02 +0100

    service completion

--- coOk!_REST_server/server/models/general-services.js
+++ coOk!_REST_server/server/models/general-services.js
@@ -60,9 +60,26 @@
 				}
 				
 				_.each(recipes, (recipe) => {
-					
-				});
+					let arr = [];
+					for (let ingredient of recipe.ingredients) {
+						let ingredient1 = ingredient.indexOf('====') > -1 ? ingredient.replace('====', '') : ingredient;
+						let ingredient2 = ingredient1.indexOf('  ') > -1 ? ingredient1.replace('  ', ' ') : ingredient1;
 
+						if (ingredient2[0] === ' ') {
+							ingredient2 = ingredient2.slice(1, ingredient2.length);
+						}
+						arr.push(ingredient2);
+					}
+					recipe.ingredients = arr;
+					
+					app.models.recipes.upsert(recipe, (err, recipe) => {
+						if (err) {
+							log.error(`[GeneralServices][modifyDbData] error: ${err}`);
+							return cb(err);
+						}
+						log.info(`[GeneralServices][modifyDbData] updated document with id: ${recipe.id}`);
+					});
+				});			
 				return cb(null,{results:'ok'});
 			});
 		}; 
