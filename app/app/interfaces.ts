567edc734f90d0cce06b4e9d1a2242346b9e455a
Christian Palazzo <info@christianpalazzo.org>
2017-01-23 17:13:15 +0100

    optionals properties in the recipe interface

--- app/app/interfaces.ts
+++ app/app/interfaces.ts
@@ -2,11 +2,11 @@
  * Recipe Interface, used to define the recipe type
  */
 export interface Recipe {
-    "name" : string,
-    "type" : string,
-    "mainIngredient" : string,
-    "persons" : number,
-    "notes" : string,
-    "ingredients" : string[],
-    "preparation" : string
+    "name": string,
+    "type": string,
+    "mainIngredient": string,
+    "persons": number,
+    "notes"?: string,
+    "ingredients": string[],
+    "preparation": string
 }
