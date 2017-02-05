327463a2e0f00b3cd266c5b6f673b1d88e50f9f3
Christian Palazzo <info@christianpalazzo.org>
2017-02-05 19:03:37 +0100

    search service file implementation

--- app/app/services.ts
+++ app/app/services.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {Device} from 'ionic-native';
 import {SQLite} from 'ionic-native';
-import {Recipe} from './interfaces';
 import 'rxjs/Rx';
 
 @Injectable()
@@ -39,37 +38,6 @@ export class DataService {
     this.http.get('./config.development.json')
     .subscribe(data => {
       return cb(data.json());
-    });
-  }
-
-  /**
-   * Get the access_token needed for authenticated calls to the back end services
-   */
-  retrieveAccessToken() {
-    return new Promise((resolve, reject) => {
-      let db = new SQLite();
-      let access_token = '';
-      db.openDatabase({
-          name: 'data.db',
-          location: 'default'
-      }).then(() => {
-        db.executeSql(`SELECT key, value FROM system WHERE key = 'access_token'`
-          , []).then((data) => {
-            if (!data.rows.length) {
-              return reject('data not found');
-            }
-            for(var i = 0; i < data.rows.length; i++) {
-                access_token = data.rows.item(i).value;
-            }
-            db.close().then(() => {
-              return resolve(access_token);
-            });
-          });
-      })
-      .catch((error) => {
-        console.error(`[retrieveAccessToken] Error: ${JSON.stringify(error)}`);
-        return reject(error);
-      });
     });
   }
 
@@ -225,61 +193,6 @@ export class DataService {
       .catch((error) => {
         console.error(`[authentication] Error: ${JSON.stringify(error)}`);
         return reject(error);
-      });
-    });
-  }
-
-  /*--------------------------------------------------------------------------------------------------------------*/
-  
-  /**
-   * Get the recipes returned from a call to the recipes back end service
-   * @param {string[]} ingredients array pushed by the user
-   * @param {Object} filters objet setted by the user
-   */
-  getRecipes(ingredients:string[]=[''],filters:any={}) {
-    return new Promise((resolve,reject) => {
-      let self = this;
-      this.retrieveConfig((config) => {
-        let url = config.authAPI.recipes;
-
-        self.retrieveAccessToken()
-        .then((access_token) => {
-          url += `?access_token=${access_token}`
-
-          if (ingredients && ingredients.length) {
-            url += `&filter[where][ingredients][regexp]=/`;
-
-            for (let ingredient of ingredients) {
-              url += `(?=.*?${ingredient})`;
-            }
-
-            url += `/i`;
-          }
-
-          if (filters && filters.recipeName) {
-            url += `&filter[where][name]=${filters.recipeName}`;
-          }
-
-          if (filters && filters.mainIngredient) {
-            url += `&filter[where][mainIngredient]=${filters.mainIngredient}`;
-          }
-
-          if (filters && filters.recipeType && filters.recipeType.length) {
-            for (let type of filters.recipeType) {
-              url += `&filter[where][type]=${type}`;
-            }
-          }
-          
-          //this.http.get(uri)
-          self.http.get('./mock.json')
-          .subscribe(data => {
-            return resolve(data.json());
-          });
-        })
-        .catch((error) => {
-          console.error(`[getRecipes] Error: ${JSON.stringify(error)}`);
-          return reject(error);
-        });
       });
     });
   }
